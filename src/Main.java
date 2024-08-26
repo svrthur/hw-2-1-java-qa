97b4fb384f34df260063c1cf96ca49429757d04d
arthur.salahutdinov <artes_93@mail.ru>
2024-08-27 01:35:25 +0300

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        int initialBalance = 300;  // измененная начальная сумма
+        int refillAmount = 1600;   // измененная сумма пополнения
+
+        int bonus = 0;
+        if (refillAmount > 1000) {
+            bonus = refillAmount / 100;  // расчет бонусов
+        }
+
+        int finalBalance = initialBalance + refillAmount + bonus;  // итоговый счет
+
+        System.out.println("Итоговый счет: " + finalBalance);
+        System.out.println("Количество бонусных рублей: " + bonus);
+    }
 }
